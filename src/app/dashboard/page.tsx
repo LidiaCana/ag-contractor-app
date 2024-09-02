bed28688918323d62df07f2024898556b1c81678
lidiaCana <in.canaless@gmail.com>
2024-09-02 11:34:11 -0400

    chore: remove unused functions for dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,37 +1,49 @@
 import * as React from 'react';
 import type { Metadata } from 'next';
+import AttendanceService from '@/api/services/attendance';
 import Grid from '@mui/material/Unstable_Grid2';
 
-// import dayjs from 'dayjs';
-
 import { config } from '@/config';
-import { Budget } from '@/components/dashboard/overview/budget';
+import { logger } from '@/lib/default-logger';
+// import { Budget } from '@/components/dashboard/overview/budget';
 // import { LatestOrders } from '@/components/dashboard/overview/latest-orders';
 // import { LatestProducts } from '@/components/dashboard/overview/latest-products';
-import { Sales } from '@/components/dashboard/overview/sales';
+// import { Sales } from '@/components/dashboard/overview/sales';
 // import { TasksProgress } from '@/components/dashboard/overview/tasks-progress';
-import { TotalCustomers } from '@/components/dashboard/overview/total-customers';
+// import { TotalCustomers } from '@/components/dashboard/overview/total-customers';
 // import { TotalProfit } from '@/components/dashboard/overview/total-profit';
 import { Traffic } from '@/components/dashboard/overview/traffic';
 
 export const metadata = { title: `Overview | Dashboard | ${config.site.name}` } satisfies Metadata;
+interface Fields {
+  'name (from group)': string[];
+}
 
-export default function Page(): React.JSX.Element {
+export default async function Page(): Promise<React.JSX.Element> {
+  let attendanceData = await AttendanceService.getAttendance<Fields>('fields%5B%5D=name (from group)');
+
+  let groupCount = attendanceData.reduce<Record<string, number>>((acc, group) => {
+    const name = group.fields['name (from group)'][0];
+    acc[name] = (acc[name] || 0) + 1;
+    return acc;
+  }, {});
+
+  logger.debug(groupCount);
   return (
     <Grid container spacing={3}>
-      <Grid lg={3} sm={6} xs={12}>
+      {/* <Grid lg={3} sm={6} xs={12}>
         <Budget diff={12} trend="up" sx={{ height: '100%' }} value="$24k" />
       </Grid>
       <Grid lg={3} sm={6} xs={12}>
         <TotalCustomers diff={16} trend="down" sx={{ height: '100%' }} value="1.6k" />
-      </Grid>
+      </Grid> */}
       {/* <Grid lg={3} sm={6} xs={12}>
         <TasksProgress sx={{ height: '100%' }} value={75.5} />
       </Grid> */}
       {/* <Grid lg={3} sm={6} xs={12}>
         <TotalProfit sx={{ height: '100%' }} value="$15k" />
       </Grid> */}
-      <Grid lg={8} xs={12}>
+      {/* <Grid lg={8} xs={12}>
         <Sales
           chartSeries={[
             { name: 'This year', data: [18, 16, 5, 8, 3, 14, 14, 16, 17, 19, 18, 20] },
@@ -39,9 +51,9 @@ export default function Page(): React.JSX.Element {
           ]}
           sx={{ height: '100%' }}
         />
-      </Grid>
+      </Grid> */}
       <Grid lg={4} md={6} xs={12}>
-        <Traffic chartSeries={[63, 15, 22]} labels={['Jade', 'AG', 'Contractor 1']} sx={{ height: '100%' }} />
+        <Traffic chartSeries={Object.values(groupCount)} labels={Object.keys(groupCount)} sx={{ height: '100%' }} />
       </Grid>
       {/* <Grid lg={4} md={6} xs={12}>
         <LatestProducts
